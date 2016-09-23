d3c586270d5c5f2eb42c33efa627036aeea1f75e
cristian.corcoveanu <cristi.corcoveanu@gmail.com>
2016-09-23 22:19:11 +0300

    Updated the way svg images are loaded

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         test: /\.scss$/,
         loaders: [ 'style', 'css?sourceMap', 'sass?sourceMap' ]
       },
-      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+      { test: /\.(png|woff|woff2|eot|ttf)$/, loader: 'url-loader?limit=100000'}
     ]
   },
   plugins: [
