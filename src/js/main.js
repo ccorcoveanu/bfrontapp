11d7ae9cc776503afa11cbf4864ad85797ff88f2
cristian.corcoveanu <cristi.corcoveanu@gmail.com>
2016-09-25 15:39:43 +0300

    Push for staging

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,19 @@
 require('../styles/main.scss')
 
+//document.addEventListener('touchstart', (evt) => {passive: true})
+
 import Hamburger from './modules/hamburger'
 
 window.onload = function() {
   const h = new Hamburger()
   document.getElementById('hamburger--link')
     .addEventListener('click', (evt) => h.toggleState(document.getElementById('hamburger--link')))
+
+  const popup_links = document.getElementsByClassName('popup--link')
+
+  for ( let i = 0; i < popup_links.length; i++ ) {
+    popup_links[i].addEventListener('click', (evt) => h.toggleState(popup_links[i]))
+  }
 }
 
 if ( module.hot ) {
