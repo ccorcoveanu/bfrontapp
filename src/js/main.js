35c5152843ae65c7462b634244791443fa7eed02
cristian.corcoveanu <cristi.corcoveanu@gmail.com>
2016-09-26 15:12:32 +0300

    Add pan to the side menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,44 @@
 require('../styles/main.scss')
+require('hammerjs')
 
-//document.addEventListener('touchstart', (evt) => {passive: true})
-
+import ClickSpeed from './modules/clickspeed'
 import Hamburger from './modules/hamburger'
 
 window.onload = function() {
+  // Optimizations
+  ClickSpeed(); // Attach touch speedup
+  document.addEventListener('touchstart', function(ev){return;}, {})
+
+  // Sidebar
+  let menu = document.getElementById('sidebar--container--element')
+  let menu_width = menu.offsetWidth
+
+  const hammerjs = new Hammer(
+    document.getElementsByClassName('resize--controller')[0],
+    {
+      direction: Hammer.DIRECTION_HORIZONTAL,
+    }
+  )
+
+  hammerjs.get('pan').set({threshold: 1})
+
+  hammerjs.on('panstart', function(ev) {
+    document.getElementsByTagName('body')[0].classList.add('js-selection-disabled')
+  })
+  hammerjs.on('panend', function(ev) {
+    document.getElementsByTagName('body')[0].classList.remove('js-selection-disabled')
+    menu_width = menu.offsetWidth
+    console.log(menu_width)
+  })
+
+  hammerjs.on('pan', function(ev) {
+    let width = menu_width + ev.deltaX
+    console.log(ev)
+    console.log(width)
+    menu.style.width = width + 'px'
+  })
+
+  // Hamburger events
   const h = new Hamburger()
   document.getElementById('hamburger--link')
     .addEventListener('click', (evt) => h.toggleState(document.getElementById('hamburger--link')))
---
+++ src/js/modules/sidebar/events.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const SidebarEvents = () => {
+  let sidebarEl = document.getElementById('sidebar--element')
+  let style = {}
+}
+
+export default SidebarEvents
