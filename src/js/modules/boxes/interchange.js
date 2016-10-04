524bcb806e991ba3e47687b22ba7d8c17bda4145
Cristian Corcoveanu <cristian.corcoveanu@ebs.msft>
2016-10-04 13:05:47 +0300

    Dragging on desktop

--- src/js/modules/boxes/interchange.js
+++ src/js/modules/boxes/interchange.js
@@ -38,6 +38,7 @@ class Interchange {
     document.addEventListener('mouseup', this.onEnd)
   }
 
+
   onStart (evt) {
     if ( this.target ) {
       return
@@ -45,6 +46,7 @@ class Interchange {
     if ( !evt.target.classList.contains(this.activeClassName) ) {
       return
     }
+
     this.target                   = evt.target.parentNode
     this.targetBCR                = this.target.getBoundingClientRect()
     this.startX                   = evt.pageX || evt.touches[0].pageX
@@ -53,6 +55,11 @@ class Interchange {
     this.currentY                 = this.startY
     this.draggingCard             = true
     this.target.style.willChange  = 'transform'
+    this.target.style.zIndex      = 260
+    this.target.style.position    = 'absolute'
+    this.initDropzone()
+
+
 
     evt.preventDefault()
   }
@@ -68,8 +75,9 @@ class Interchange {
     if (!this.target)
       return
 
+    this.targetX = 0
+    this.targetY = 0
     this.draggingCard = false
-    this.target = null
   }
 
   update () {
@@ -79,15 +87,49 @@ class Interchange {
       return
 
     if (this.draggingCard) {
-      console.log(this.currentX)
-      console.log(this.startX)
-      this.screenX = this.currentX - this.startX
+      this.screenX = this.currentX - this.startX - this.targetBCR.width-10
       this.screenY = this.currentY - this.startY
     } else {
-
+      this.screenX += (this.targetX - this.screenX - this.targetBCR.width -10) / 4
+      this.screenY += (this.targetY - this.screenY) / 4
     }
 
+    let calcScreenX = this.screenX + this.targetBCR.width + 10
+
+    const isNearlyAtStart = (Math.abs(calcScreenX) < 0.1 && Math.abs(this.screenY) < 0.1) && this.draggingCard === false
     this.target.style.transform = `translate(${this.screenX}px, ${this.screenY}px)`
+
+    if ( isNearlyAtStart ) {
+      this.resetTarget()
+      this.destroyDropzone()
+    }
+  }
+
+  initDropzone() {
+    this.overlay.style.position = 'relative'
+    this.overlay.style.width = this.targetBCR.width + 'px'
+    this.overlay.style.height = this.targetBCR.height + 'px'
+    this.overlay.parentNode.insertBefore(this.overlay, this.target)
+
+    this.overlay.style.opacity = 1
+  }
+
+  destroyDropzone() {
+    this.overlay.style.width = 0
+    this.overlay.style.height = 0
+    this.overlay.style.opacity = 0
+    this.overlay.style.position = 'absolute'
+  }
+
+  resetTarget() {
+    if (!this.target)
+      return
+
+    this.target.style.willChange = 'initial'
+    this.target.style.transform = 'none'
+    this.target.style.zIndex = 250
+    this.target.style.position = 'relative'
+    this.target = null
   }
 }
 
